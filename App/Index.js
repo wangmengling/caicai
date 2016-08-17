262b3596d719b158b44a76d39f5b08f1c7ec24ad
wangmengling <294025529@qq.com>
2016-08-17 22:56:34 +0800

    pdf

--- App/Index.js
+++ App/Index.js
@@ -1,3 +1,18 @@
+// import Home from './Components/Home';
+// import HomeRoute from './Routes/HomeRoute';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import Relay from 'react-relay';
+//
+// ReactDOM.render(
+//   <Relay.RootContainer
+//     Component={Home}
+//     route={new HomeRoute()}
+//   />,
+//   document.getElementById('root')
+// );
+
+import 'babel-polyfill';
 import Home from './components/Home';
 import HomeRoute from './routes/HomeRoute';
 import React from 'react';
--- App/Routes/HomeRoute.js
+++ App/Routes/HomeRoute.js
@@ -3,16 +3,22 @@ import Relay from 'react-relay';
 
 export default class extends Relay.Route {
   static routeName = 'HomeRoute';  // A unique name
+
+
   static queries = {
-    // Here, we compose your Relay container's
-    // 'greetings' fragment into the 'greetings'
-    // field at the root of the GraphQL schema.
-    greetings: (Component) => Relay.QL`
-      query GreetingsQuery {
-        greetings {
-          ${Component.getFragment('greetings')},
-        },
-      }
-    `,
+    greetings: () => Relay.QL`query { greetings }`,
   };
+
+  // static queries = {
+  //   // Here, we compose your Relay container's
+  //   // 'greetings' fragment into the 'greetings'
+  //   // field at the root of the GraphQL schema.
+  //   greetings: (Component) => Relay.QL`
+  //     query GreetingsQuery {
+  //       greetings {
+  //         ${Component.getFragment('greetings')},
+  //       },
+  //     }
+  //   `,
+  // };
 }
